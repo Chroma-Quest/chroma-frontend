a2b595906e1e059c2ea1237aff6da00ed2143d7b
Brock Britton <brockbritton@outlook.com>
2024-03-19 10:32:06 -0700

    added keys to piano

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
+import React from 'react';
 import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import './styles/App.css';
 import './styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Simulator from './Simulator.jsx';
 
 
 function App() {
@@ -13,6 +20,13 @@ function App() {
   return (
     <>
       <Header />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<p>home</p>} />
+          <Route exact path="/simulator" element={<Simulator />} />
+          <Route exact path="/about" element={<p>about</p>} />
+        </Routes>
+      </Router>
       <Footer />
     </>
   )
