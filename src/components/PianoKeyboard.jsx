76f07f122b0d82cced4603b71d7617acda4dead6
Brock Britton <brockbritton@outlook.com>
2024-03-20 11:39:41 -0700

    working on linking canvas and piano

--- src/components/PianoKeyboard.jsx
+++ src/components/PianoKeyboard.jsx
@@ -16,59 +16,71 @@ import B5 from '../assets/piano-sounds/mp3/Piano.ff.B5.mp3';
 import C6 from '../assets/piano-sounds/mp3/Piano.ff.C6.mp3';
 
 
-const PianoKeyboard = () => {
+const labels_colors_notes = {
+  'a': {
+    color: "#FFFF00",
+    sound: C5,
+  },
+  'w': {
+    color: "#00FFFF",
+    sound: Csharp5,
+  },
+  's': {
+    color: "#228B22",
+    sound: D5,
+  },
+  'e': {
+    color: "#DC143C",
+    sound: Csharp5,
+  },
+  'd': {
+    color: "#87CEEB",
+    sound: E5,
+  },
+  'f': {
+    color: "#9370DB",
+    sound: F5,
+  },
+  't': {
+    color: "#DAA520",
+    sound: Fsharp5,
+  },
+  'g': {
+    color: "#008000",
+    sound: G5,
+  },
+  'y': {
+    color: "#FF6F61",
+    sound: Gsharp5,
+  },
+  'h': {
+    color: "#FF8C00",
+    sound: A5,
+  },
+  'u': {
+    color: "#4B0082",
+    sound: Asharp5,
+  },
+  'j': {
+    color: "#FF007F",
+    sound: B5,
+  },
+  'k': {
+    color: "#FFFF00",
+    sound: C6,
+  },
+
+}
+
+const PianoKeyboard = (props) => {
   
-  const handleKeyDown = (event) => {
-    switch (event.key) {
-      case 'a':
-        console.log('a')
-        new Audio(C5).play();
-        break;
-      case 'w':
-        new Audio(Csharp5).play();
-        break;
-      case 's':
-        console.log('s')
-        new Audio(D5).play();
-        break;
-      case 'e':
-        new Audio(Dsharp5).play();
-        break;
-      case 'd':
-        new Audio(E5).play();
-        break;
-      case 'f':
-        new Audio(F5).play();
-        break;
-      case 't':
-        new Audio(Fsharp5).play();
-        break;
-      case 'g':
-        new Audio(G5).play();
-        break;
-      case 'y':
-        new Audio(Gsharp5).play();
-        break;
-      case 'h':
-        new Audio(A5).play();
-        break;
-      case 'u':
-        new Audio(Asharp5).play();
-        break;
-      case 'j':
-        new Audio(B5).play();
-        break;
-      case 'k':
-        new Audio(C6).play();
-        break;
-      case ' ':
-        console.log("firework")
-        break
-      default:
-        console.log("NOT DEFINED");
-        console.log(event.key)
-        break;
+  const handleKeyDown = async (event) => {
+    if (keyBinds.includes(event.key)) {
+      
+      new Audio(labels_colors_notes[event.key]['sound']).play();
+      props.setColorsQueue([...props.colorsQueue, labels_colors_notes[event.key]['color']]);
     }
+    
   }
 
   const handleClick = (key) => {
@@ -83,7 +95,7 @@ const PianoKeyboard = () => {
   const whiteKeys = ["C", "D", "E", "F", "G", "A", "B", "C6"];
   const blackKeys = ["C#", "D#", "", "F#", "G#", "A#", ""];
 
-  const keyBind = ["a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j"];
+  const keyBinds = ["a", "w", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j", "k"];
   const topKeyBind = ["w", "e", "", "t", "y", "u",]
   const bottomKeyBind = ["a", "s", "d", "f", "g", "h", "j", "k"]
   const keyColors = ["#FFFF00", "#00FFFF", "#228B22", "#DC143C", "#87CEEB", "#9370DB", "#DAA520", "#008000", "#FF6F61", "#FF8C00", "#4B0082", "#FF007F", "#FFFF00"]
