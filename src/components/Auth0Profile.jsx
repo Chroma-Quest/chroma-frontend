55cd88a72437ca4c260fe8954336b4f3119feff7
JOHN T GAUSE III <johntgause12@gmail.com>
2024-03-21 08:55:59 -0700

    auth0

---
+++ src/components/Auth0Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div>
+        <h2>Welcome, {user.name}</h2>
+        <p>Email: {user.email}</p>
+      </div>
+    )
+  );
+};
+
+export default Profile;
