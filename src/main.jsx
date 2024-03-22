14142754ce0d06d8ddb7494ed965b71e6e9e34d2
JOHN T GAUSE III <johntgause12@gmail.com>
2024-03-22 02:39:22 -0700

    more fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
-// import Auth from './components/Auth0.jsx'; 
 import './styles/index.css';
 import './styles/App.css';
 
 const CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
-// eslint-disable-next-line react-refresh/only-export-components
 const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
-const REDIRCT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI;
-// const AUDIENCE = import.meta.env.VITE_AUTH0_AUDI_ID;
+const REDIRECT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain = {DOMAIN}
       clientId= {CLIENT_ID}
       authorizationParams={{
-        redirect_uri:REDIRCT_URI
+        redirect_uri:REDIRECT_URI
       }}
       // audience= {AUDIENCE}
     >
@@ -26,3 +23,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Auth0Provider>
   </React.StrictMode>,
 )
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import { Auth0Provider } from '@auth0/auth0-react';
+// // import Auth from './components/Auth0.jsx'; 
+// import './styles/index.css';
+// import './styles/App.css';
+
+// const CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+// const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+// const REDIRECT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+// // const AUDIENCE = import.meta.env.VITE_AUTH0_AUDI_ID;
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Auth0Provider
+//       domain = {DOMAIN}
+//       clientId= {CLIENT_ID}
+//       authorizationParams={{
+//         redirect_uri:REDIRECT_URI
+//       }}
+//       // audience= {AUDIENCE}
+//     >
+//       <App />
+//     </Auth0Provider>
+//   </React.StrictMode>,
+// )
