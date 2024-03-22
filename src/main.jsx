7e7c97be07e533182b8b53274e01661a0760f111
JOHN T GAUSE III <johntgause12@gmail.com>
2024-03-22 01:55:11 -0700

    almost working i think

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Auth from './components/Auth0.jsx'; 
+// import Auth from './components/Auth0.jsx'; 
 import './styles/index.css';
 import './styles/App.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
+const REDIRCT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI;
+// const AUDIENCE = import.meta.env.VITE_AUTH0_AUDI_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth />
-    <App />
+    <Auth0Provider
+      domain = {DOMAIN}
+      clientId= {CLIENT_ID}
+      authorizationParams={{
+        redirect_uri:REDIRCT_URI
+      }}
+      // audience= {AUDIENCE}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
